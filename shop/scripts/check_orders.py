0c5e5f4d98a0d5f7fcc911a88fef573356c55b8a
Ruiner777 <dobrodomovv@gmail.com>
2026-01-04 01:49:56 +0300

    relocate files and delete duplicates

--- shop/scripts/check_orders.py
+++ shop/scripts/check_orders.py
@@ -5,11 +5,18 @@ import django
 
 # Настройка Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
-sys.path.append('shop')
 django.setup()
 
 from orders.models import Order
 from users.models import User
+
+print(f'Всего заказов: {Order.objects.count()}')
+print(f'Заказов с пользователем: {Order.objects.filter(user__isnull=False).count()}')
+print(f'Заказов без пользователя: {Order.objects.filter(user__isnull=True).count()}')
+
+print('\nПоследние 5 заказов:')
+for order in Order.objects.all()[:5]:
+    print(f'ID: {order.id}, User: {order.user}, Created: {order.created}, Email: {order.email}')
 
 print('Пользователи с заказами:')
 users_with_orders = User.objects.filter(order__isnull=False).distinct()
